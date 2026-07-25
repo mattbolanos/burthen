b3c3612fea897108daa83c16ff684cb84f705ff4
mattbolanos <matthew.a.bolanos@gmail.com>
2026-07-25 17:28:24 -0400

    initial commit

--- Tonnage/ContentView.swift
+++ Tonnage/ContentView.swift
@@ -25,9 +25,6 @@ struct ContentView: View {
                 .onDelete(perform: deleteItems)
             }
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
                 ToolbarItem {
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
